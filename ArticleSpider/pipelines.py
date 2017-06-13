a93f01fed52d1fe75159fc74ea2a5b15d291708c
OneJ <3942241@qq.com>
2017-06-14 01:39:34 +0800

    +

--- ArticleSpider/pipelines.py
+++ ArticleSpider/pipelines.py
@@ -7,8 +7,10 @@
 import codecs
 import json
 import MySQLdb
+import MySQLdb.cursors
 from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exporters import JsonItemExporter
+from twisted.enterprise import adbapi
 
 class ArticlespiderPipeline(object):
     def process_item(self, item, spider):
@@ -45,6 +47,41 @@ class MysqlPipeline(object):
 
         self.conn.commit()
 
+class MysqltwistedPipline(object):
+    def __init__(self,dbpool):
+        self.dbpool = dbpool
+
+    @classmethod
+    def from_settings(cls,settings):
+        dbparms = dict(
+            host=settings["MYSQL_HOST"],
+            db=settings["MYSQL_DBNAME"],
+            user=settings["MYSQL_USER"],
+            passwd=settings["MYSQL_PASSWORD"],
+            charset='utf8',
+            cursorclass=MySQLdb.cursors.DictCursor,
+            use_unicode=True,
+        )
+        dbpool = adbapi.ConnectionPool("MySQLdb", **dbparms)
+        return cls(dbpool)
+
+    def process_item(self, item, spider):
+        # 使用twisted将mysql插入变成异步执行
+        query = self.dbpool.runInteraction(self.do_insert, item)
+        query.addErrback(self.handle_error, item, spider)  # 处理异常
+
+    def handle_error(self,failure):
+        print(failure)
+
+    def do_insert(self,cursor,item):
+        insert_sql = """
+                    insert into article(url_object_id, title, url, fav_nums,content,tags,create_date,praise_nums)
+                    VALUES (%s,%s, %s, %s, %s,%s,%s,%s)
+                """
+        cursor.execute(insert_sql, (item["url_object_id"], item["title"],
+                                         item["url"], item["fav_nums"], item["content"],
+                                         item["tags"], item["create_date"], item["praise_nums"]))
+
 class JsonExporterPipeline(object):
     #调用scrapy提供的jsonexporter导出json文件
     def __init__(self):
@@ -62,7 +99,8 @@ class JsonExporterPipeline(object):
 
 class ArticleImagePipeline(ImagesPipeline):
     def item_completed(self, results, item, info):
-        for ok,value in results:
-            image_file_path = value["path"]
-        item["front_image_path"] = image_file_path
+        if "front_image_url" in item:
+            for ok, value in results:
+                image_file_path = value["path"]
+                item["front_image_path"] = image_file_path
         return item
