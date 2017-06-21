122876d171a6e49c4785395d4a2ee28c12d7fe5d
OneJ <3942241@qq.com>
2017-06-22 00:30:32 +0800

    +

--- ArticleSpider/spiders/lagou.py
+++ ArticleSpider/spiders/lagou.py
@@ -7,13 +7,16 @@ from scrapy.spiders import CrawlSpider, Rule
 class LagouSpider(CrawlSpider):
     name = 'lagou'
     allowed_domains = ['www.lagou.com']
-    start_urls = ['http://www.lagou.com/']
+    start_urls = ['https://www.lagou.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'zhaopin/*.html'),follow=True),
+        Rule(LinkExtractor(allow=r'gongsi/j\d+.html'),follow=True),
+        Rule(LinkExtractor(allow=r'jobs/\d+.html'), callback='parse_job', follow=True),
     )
 
-    def parse_item(self, response):
+    def parse_job(self, response):
+        # 解析拉钩网职位
         i = {}
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
